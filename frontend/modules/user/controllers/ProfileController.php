5bd456336a48788d9a33dabb942defde6288e5db
Zharkov Andrey <zharkov.php@gmail.com>
2017-09-15 21:04:51 +0300

    yii-faker | создание тестовых пользователей с плагином yii2-faker

--- frontend/modules/user/controllers/ProfileController.php
+++ frontend/modules/user/controllers/ProfileController.php
@@ -38,4 +38,23 @@ class ProfileController extends Controller
         throw new NotFoundHttpException();
     }
 
+        public function actionGenerate()
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 1000; $i++) {
+            $user = new User([
+                'username' => $faker->name,
+                'email' => $faker->email,
+                'about' => $faker->text(200),
+                'nickname' => $faker->regexify('[A-Za-z0-9_]{5,15}'),
+                'auth_key' => Yii::$app->security->generateRandomString(),
+                'password_hash' => Yii::$app->security->generateRandomString(),
+                'created_at' => $time = time(),
+                'updated_at' => $time,
+            ]);
+            $user->save(false);
+       }
+    }
+
 }
