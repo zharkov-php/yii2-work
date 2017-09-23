f8035b641f7724769430056fd2d4a2612ceb68c2
Zharkov Andrey <zharkov.php@gmail.com>
2017-09-23 17:07:32 +0300

    Доработка загрузки изображения, заливка одинакового размера картинки(если размер превышает)библиотека Intervention image

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -4,4 +4,9 @@ return [
     'maxFileSize' => 1024 * 1024 * 2, // 2 megabites
     'storagePath' => '@frontend/web/uploads/',
     'storageUri' => '/uploads/',   // http://images.com/uploads/f1/d7/739f9a9c9a99294.jpg
+    // Настройки могут быть вложенными
+        'profilePicture' => [
+                'maxWidth' => 200,
+                'maxHeight' => 300,
+            ],
 ];
--- frontend/modules/user/models/forms/PictureForm.php
+++ frontend/modules/user/models/forms/PictureForm.php
@@ -10,6 +10,7 @@ namespace frontend\modules\user\models\forms;
 
 use Yii;
 use yii\base\Model;
+use Intervention\Image\ImageManager;
 
 class PictureForm extends Model
 {
@@ -26,6 +27,33 @@ class PictureForm extends Model
         ];
     }
 
+    public function __construct()
+    {
+        $this->on(self::EVENT_AFTER_VALIDATE, [$this, 'resizePicture']);
+    }
 
+    /**
+     * Resize image if needed
+     */
+    public function resizePicture()
+    {
+            $width = Yii::$app->params['profilePicture']['maxWidth'];
+            $height = Yii::$app->params['profilePicture']['maxHeight'];
+
+            $manager = new ImageManager(array('driver' => 'imagick'));
+
+            $image = $manager->make($this->picture->tempName);
+
+            // 3-й аргумент - органичения - специальные настройки при изменении размера
+            $image->resize($width, $height, function ($constraint) {
+
+                    // Пропорции изображений оставлять такими же (например, для избежания широких или вытянутых лиц)
+                    $constraint->aspectRatio();
+
+                    // Изображения, размером меньше заданных $width, $height не будут изменены:
+                    $constraint->upsize();
+
+                })->save();
+        }
 
 }
